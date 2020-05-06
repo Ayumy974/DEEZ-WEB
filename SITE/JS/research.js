df95672237bfe98af0b837c6076142fed057b364
fklock <francoiseklock@gmail.com>
2020-05-06 09:39:01 +0200

    delete the old selection and implement sorting

--- SITE/JS/research.js
+++ SITE/JS/research.js
@@ -1,113 +1,63 @@
 
 $(document).ready(function () {
     
-    // $('.input_research').change(function () {
-    //     const TITLE = $('.input_research').val();
-    //     console.log(TITLE);    
-    // })
-    // const researchTitle = `${TITLE}`;
+    $('.input_research').focus(function () {
+        console.log('changed');
+        $(this).val('');
+    });
 
     $('.research_area').submit((e) => {
         e.preventDefault();
+        $('#music-cards').empty();
+    });
+    
+    $('.research_area').submit((e) => {
+        e.preventDefault();
         const TITLE = $('.input_research').val();
-        // console.log(TITLE);
-
+        
         // Afficher le texte du champ sélectionné:
         let selectedCriteria = "";
         $('#criteria').change(function () {
-            let str = '';
             $("select option:selected").each(function () {
-                str += $(this).val() + " ";
-                selectedCriteria = str;
+                selectedCriteria += $(this).val() + " ";
             });
         }).change();
     
         console.log(selectedCriteria);
-        if ($("select option:selected").val() === 'album') {
-            $.ajax({
-                url: `https://api.deezer.com/search?q=${selectedCriteria}:"${TITLE}"&output=jsonp`,
-                method: 'GET',
-                dataType: 'jsonp'
-            })
-            .then(result => {
-                console.log(result);
-                // console.log(element.artist.preview)
-                result.data.forEach(element => {
-                    $('#music-cards').append(
-                        `
-                        <div class='card'>
-                            <div class='mini_bg' style="background-image: url('${element.album.cover}');"></div>
-                            <audio controls src=${element.preview}></audio>
-                            <h5>Album: ${element.album.title}</h5>
-                        </div>
-                    `)
-                })
-            })
-        } else if ($("select option:selected").val() === 'artist') {
-            $.ajax({
-                url: `https://api.deezer.com/search?q=${selectedCriteria}:"${TITLE}"&output=jsonp`,
-                method: 'GET',
-                dataType: 'jsonp'
-            })
-            .then(result => {
-                console.log(result);
-                result.data.forEach(element => {
-                    $('#music-cards').append(
-                        `
-                        <div class='card'>
-                            <div class='mini_bg' style="background-image: url('${element.artist.picture}');"></div>
-                            <div class='overlay'></div>
-                            <h3>Nom de l'artiste: ${element.artist.name}</h3>
-                            <audio controls src=${element.artist.preview}></audio>  
-                        </div>
-                        `)
-                })
-            })
-        }
-        else {
+        // if ($("select option:selected").val() === 'album') {
+        $.ajax({
+            url: `https://api.deezer.com/search?q=${TITLE}&order=${selectedCriteria}&output=jsonp`,
+            method: 'GET',
+            dataType: 'jsonp'
+        })
+        .then(result => {
+            console.log(result);
+            // console.log(element.artist.preview)
+            result.data.forEach(element => {
                 $('#music-cards').append(
-                    `<h2>Aucun résultat pour cette recherche</h2>`
-                )
-            }
+                    `
+                    <ul class='card'>
+                        <div class='mini_bg' style="background-image: url('${element.album.cover}');"></div>
+                        <li><audio controls src=${element.preview}></audio></li>
+                        <li>Titre:${element.title_short}</li>
+                        <li>Artiste: ${element.artist.name}</li>
+                        <li>Album:${element.album.title}</li>    
+                    </ul>
+                `)
+            })
+        }) 
     })
+    
+    $('#music-cards').empty();
+    
+
+    
+
+    //     else {
+    //             $('#music-cards').append(
+    //                 `<h2>Aucun résultat pour cette recherche</h2>`
+    //             )
+    //         }
+    // })
 })
-                    // // CASE ARTIST:
-                    // else if (selectedCriteria == 'artist') {
-                    //     console.log('ok artist')
 
-                    //     $('#music-cards').append(
-                    //         `
-                    //     <div class='card'>
-                    //         <div class='mini_bg' style="background-image: url('${element.picture}');"></div>
-                    //         <div class='overlay'></div>
-                    //         <ul>
-                    //             <li>Nom de l'artiste: ${element.artist.name}</li>
-                    //         </ul>
-                    //     </div>
-                    //     `)
-                    // }
-                    // // CASE MUSIC:
-                    // else if (selectedCriteria === 'track') {
-                    //     console.log('ok musique')
-                    //     $('#music-cards').append(
-                    //         `
-                    //     <div class='card'>
-                    //         <div class='mini_bg' style="background-image: url('${element.artist.preview}');"></div>
-                    //         <div class='overlay'></div>
-                    //         <ul>
-                    //             <li>Nom de la musique: ${element.artist.title}</li>
-                    //         </ul>
-                    //     </div>
-                    //     `)
-                    // } else {
-                    //     $('#music-cards').append(
-                    //         `<h2>Aucun résultat pour cette recherche</h2>`
-                    //     )
-                    // }
-                // });       
-
-// const displayAlbum = album => {
-//     album.forEach( title => {
-//         console.log(title)
-//     });
-// }
