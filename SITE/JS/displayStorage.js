57684572a641101b9cae751d8b1804471f9f6659
fklock <francoiseklock@gmail.com>
2020-05-08 00:10:49 +0200

    changing the text button onclick + resolved display problem on home page

--- SITE/JS/displayStorage.js
+++ SITE/JS/displayStorage.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function () {
     const music_data = JSON.parse(localStorage.getItem('favorites'));
     console.log(music_data);
-    if(music_data !== []) {
+    if(music_data !== null) {
         music_data.forEach(element => {
                 $(".home_favorites").append(
                     `
@@ -9,7 +9,7 @@ jQuery(document).ready(function () {
                         <li class='mini_bg' style="background-image: url('${element.cover}');"></li>
                         <li><audio controls src=${element.audio}></audio></li>
                         <li id='title_music'>Titre:${element.titleshort}</li>
-                        <li class='artist-name'>Artiste: ${element.author}</li>
+                        <li class='artist-name'>Artiste:${element.artist}</li>
                         <li class='album-title'>Album:${element.title}</li> 
                         <button class='favorites'type='button' >Ajout de favoris</button>
                     </ul>
--- SITE/JS/research.js
+++ SITE/JS/research.js
@@ -1,172 +1,176 @@
- // Création de mon tableau de favoris du local storage:
 
-function addToFavorite(sender) {
-    let favoritesArray = localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites')) : [];
-    const musicId = parseInt($(sender).parent().attr('id'));
-    let found = false;
-    favoritesArray.forEach(function (item, index, object) {
-        if (item.id === musicId) {
-            object.splice(index, 1);
-            found = true;
+// Implémentation de la fonction addToFavorite
+    // LOCALSTORAGE:
+
+    function addToFavorite(sender) {
+        let favoritesArray = localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites')) : [];
+        const musicId = parseInt($(sender).parent().attr('id'));
+        let found = false;
+        favoritesArray.forEach(function (item, index, object) {
+            if (item.id === musicId) {
+                object.splice(index, 1);
+                found = true;
             }
-        }
-    )
-    if (found === false) {
-        favoritesArray.push({
-            id: musicId,
-            cover: $(sender).parent().data('cover'),
-            audio: $(sender).parent().data('audio'),
-            titleshort: $(sender).parent().data('titleshort'),
-            author: $(sender).parent().data('author'),
-            title: $(sender).parent().data('title'),
-        });
-    }
-
+        })
+        if (found === false) {
+            favoritesArray.push({
+                id: musicId,
+                cover: $(sender).parent().data('cover'),
+                audio: $(sender).parent().data('audio'),
+                artist: $(sender).parent().data('artist'),
+                titleshort: $(sender).parent().data('titleshort'),
+                title: $(sender).parent().data('title'),
+            }); 
+            $(sender).text('Supprimer des favoris');
+                
+        } else { $(sender).text('Ajouter aux favoris'); }
     localStorage.setItem('favorites', JSON.stringify(favoritesArray));
     console.log(favoritesArray);
 }
 
-jQuery(document).ready(function () {
-    // remettre l'input de recherche à zéro.
-    $('.input_research').focus(function () {
-        // console.log('changed');
-        $(this).val('');
-    });
+    // Utilisation de jQuery pour la requête AJAX vers l'API Deezer.
+    jQuery(document).ready(function () {
+        // remettre l'input de recherche à zéro.
+        $('.input_research').focus(function () {
+            // console.log('changed');
+            $(this).val('');
+        });
 
-    // Effacer les anciennes rechercher au submit qui sont remplacées par les nouvelles:
-    $('.research_area').submit((e) => {
-        e.preventDefault();
-        $('#music-cards').empty();
-    });
-    
-    
-    // SOUMISSION du formulaire de recherche
-        // TODO 1: Récupérer la valeur du champ de recherche.
-        // TODO 2: Détecter le changement sur l'input select etrécupérer la valeur.
-        // TODO 3: En fonction du critère choisi, requête ajax pour récupérer les données et les afficher.
-
-    $('.research_area').submit((e) => {
-        e.preventDefault();
-
-        // 1- Valeur de mon input de recherche queje passe à l'url de l'API.
-        const TITLE = $('.input_research').val();
-
-        // 2 -CHAMP SELECT pour le tri, que je passe à l'url de l'API.
-        let selectedCriteria = "";
-        $('#criteria').change(function () {
-            $("select option:selected").each(function () {
-                selectedCriteria += $(this).val() + " ";
-            });
-        }).change();
-    
-        console.log(selectedCriteria);
-
-        const selectedValue = $("select option:selected").val();
-
-        // 3- En fonction de la sélection et de la valeur de mon champ, je fais une requête AJAX et j'affiche les résultats.
-
-        // Récupérer le local storage: construire un array simple contenant uniquement les id des favoris
-        const music_data = JSON.parse(localStorage.getItem('favorites'));
-        let favoritesIds = [];
-        if (music_data !== null) {
-            favoritesIds = music_data.map(el => {
-                return el.id;
-            });   
-        }
-        console.log(favoritesIds);
-        // FIRST CONDITION: CASE album ou CASE artist ou CASE Music
-        if (selectedValue === 'album' || selectedValue === 'artist' || selectedValue === 'track') {
-            $.ajax({
-                url: `https://api.deezer.com/search?q=${TITLE}&order=${selectedCriteria}&output=jsonp`,
-                method: 'GET',
-                dataType: 'jsonp'
-            })
-                .then(result => {
-                    console.log(result);
-
-                    result.data.forEach( element => {
-                        $('#music-cards').append(
-                            `
-                        <ul
-                            class='card'
-                            id = ${element.id}  
-                            data-cover = "${element.album.cover}"
-                            data-audio = "${element.preview}"
-                            data-titleshort = "${element.title_short}" 
-                            data-artist = "${element.artist.name}" 
-                            data-title = "${element.album.title}" 
-                        >
-                            <li id='mini_bg' style="background-image: url('${element.album.cover}');">
-                            </li>
-                            <li><audio preload="auto" controls src=${element.preview}></audio></li>
-                            <li id='title_music'><span>Titre: </span>"${element.title_short}"</li>
-                            <li class='artist-name'><span>Artiste: </span>"${element.artist.name}"</li>
-                            <li class='album-title'><span>Album: </span>"${element.album.title}"</li> 
-                            <button onclick="addToFavorite(this)" class='btn_favorites' type='button' >${favoritesIds.includes(element.id) ? 'Supprimer' : 'Ajouter'}</button>
-                        </ul>
-                    `)
-                    })
-
-                    // Fonction pour le player audio venant d'un plugin:
-                    $(function () {
-                        $('audio').audioPlayer();
-                    });
-                }) 
-            // .catch( error => console.log(error))
-        }
+        // Effacer les anciennes rechercher au submit qui sont remplacées par les nouvelles:
+        $('.research_area').submit((e) => {
+            e.preventDefault();
+            $('#music-cards').empty();
+        });
         
-        // SECOND CONDITION: the most popular (fan)
-        // else if (selectedValue === 'top') {
-        //     $.ajax({
-        //         url: `https://api.deezer.com/search?q=${TITLE}/top&order=${selectedCriteria}&output=jsonp`,
-        //         method: 'GET',
-        //         dataType: 'jsonp'
-        //     })
-        //     console.log(result);
-        //     result.data.forEach(element => {
-        //         $('#music-cards').append(
-        //             `
-        //             <ul class='card'>
-        //                 <div class='mini_bg' style="background-image: url('${element.album.cover}');"></div>
-        //                 <li><audio controls src=${element.preview}></audio></li>
-        //                 <li>Titre:${element.title_short}</li>
-        //                 <li>Artiste: ${element.artist.name}</li>
-        //                 <li>Album:${element.album.title}</li>    
-        //             </ul>
-        //         `)
-        //     })
-        // }
+        
+        // SOUMISSION du formulaire de recherche
+            // TODO 1: Récupérer la valeur du champ de recherche.
+            // TODO 2: Détecter le changement sur l'input select etrécupérer la valeur.
+            // TODO 3: En fonction du critère choisi, requête ajax pour récupérer les données et les afficher.
 
-        // THIRD CONDITION: rank order
-        else if (selectedValue === 'rank') {
-            $.ajax({
-                url: `https://api.deezer.com/search?q=${TITLE}&order=${selectedCriteria}&output=jsonp`,
-                method: 'GET',
-                dataType: 'jsonp'
-            })
-            .then(result => {
-                const popularMusic = result.data.map(music => music.rank);
-                console.log(popularMusic);
-                const sortArr = popularMusic.sort(function (a, b) { return a - b }); // tri par ordre croissant
-                console.log(sortArr);
-                result.data.forEach( (element, i) => {
-                    console.log(element.rank = sortArr[i]);
-                    console.log(result.data);
-                    if (element.rank = sortArr[i]) {
-                        $('#music-cards').append(
-                            `
-                            <ul class='card'>
-                                <div class='mini_bg' style="background-image: url('${element.album.cover}');"></div>
-                                <li><audio controls src=${element.preview}></audio></li>
-                                <li>Titre:${element.title_short}</li>
-                                <li>Artiste: ${element.artist.name}</li>
-                                <li>Album:${element.album.title}</li>    
+        $('.research_area').submit((e) => {
+            e.preventDefault();
+
+            // 1- Valeur de mon input de recherche queje passe à l'url de l'API.
+            const TITLE = $('.input_research').val();
+
+            // 2 -CHAMP SELECT pour le tri, que je passe à l'url de l'API.
+            let selectedCriteria = "";
+            $('#criteria').change(function () {
+                $("select option:selected").each(function () {
+                    selectedCriteria += $(this).val() + " ";
+                });
+            }).change();
+        
+            console.log('Le critère sélectionné: ' + selectedCriteria);
+
+            const selectedValue = $("select option:selected").val();
+
+            // 3- En fonction de la sélection et de la valeur de mon champ, je fais une requête AJAX et j'affiche les résultats.
+
+            // Récupérer le local storage: construire un array simple contenant uniquement les id des favoris
+            const music_data = JSON.parse(localStorage.getItem('favorites'));
+            let favoritesIds = [];
+            if (music_data !== null) {
+                favoritesIds = music_data.map(el => {
+                    return el.id;
+                });   
+            }
+            console.log('Mon array d\'id de favoris: ' + favoritesIds);
+
+            // FIRST CONDITION: CASE album ou CASE artist ou CASE Music
+            if (selectedValue === 'album' || selectedValue === 'artist' || selectedValue === 'track') {
+                $.ajax({
+                    url: `https://api.deezer.com/search?q=${TITLE}&order=${selectedCriteria}&output=jsonp`,
+                    method: 'GET',
+                    dataType: 'jsonp'
+                })
+                    .then(result => {
+                        console.log(result);
+
+                        result.data.forEach( element => {
+                            $('#music-cards').append(
+                                `
+                            <ul
+                                class='card'
+                                id = ${element.id}  
+                                data-cover = "${element.album.cover}"
+                                data-audio = "${element.preview}"
+                                data-titleshort = "${element.title_short}" 
+                                data-artist = "${element.artist.name}" 
+                                data-title = "${element.album.title}" 
+                            >
+                                <li id='mini_bg' style="background-image: url('${element.album.cover}');">
+                                </li>
+                                <li><audio preload="auto" controls src=${element.preview}></audio></li>
+                                <li id='title_music'><span>Titre: </span>"${element.title_short}"</li>
+                                <li class='artist-name'><span>Artiste: </span>"${element.artist.name}"</li>
+                                <li class='album-title'><span>Album: </span>"${element.album.title}"</li> 
+                                <button onclick="addToFavorite(this)" class='btn_favorites' type='button' >${favoritesIds.includes(element.id) ? 'Supprimer des favoris' : 'Ajouter aux favoris'}</button>
                             </ul>
                         `)
-                    }
+                        })
+
+                        // Fonction pour le player audio venant d'un plugin:
+                        $(function () {
+                            $('audio').audioPlayer();
+                        });
+                    }) 
+                // .catch( error => console.log(error))
+            }
+            
+            // SECOND CONDITION: the most popular (fan)
+            // else if (selectedValue === 'top') {
+            //     $.ajax({
+            //         url: `https://api.deezer.com/search?q=${TITLE}/top&order=${selectedCriteria}&output=jsonp`,
+            //         method: 'GET',
+            //         dataType: 'jsonp'
+            //     })
+            //     console.log(result);
+            //     result.data.forEach(element => {
+            //         $('#music-cards').append(
+            //             `
+            //             <ul class='card'>
+            //                 <div class='mini_bg' style="background-image: url('${element.album.cover}');"></div>
+            //                 <li><audio controls src=${element.preview}></audio></li>
+            //                 <li>Titre:${element.title_short}</li>
+            //                 <li>Artiste: ${element.artist.name}</li>
+            //                 <li>Album:${element.album.title}</li>    
+            //             </ul>
+            //         `)
+            //     })
+            // }
+
+            // THIRD CONDITION: rank order
+            else if (selectedValue === 'rank') {
+                $.ajax({
+                    url: `https://api.deezer.com/search?q=${TITLE}&order=${selectedCriteria}&output=jsonp`,
+                    method: 'GET',
+                    dataType: 'jsonp'
                 })
-            })
-        }
-    })   
-})
+                .then(result => {
+                    const popularMusic = result.data.map(music => music.rank);
+                    console.log(popularMusic);
+                    const sortArr = popularMusic.sort(function (a, b) { return a - b }); // tri par ordre croissant
+                    console.log(sortArr);
+                    result.data.forEach( (element, i) => {
+                        console.log(element.rank = sortArr[i]);
+                        console.log(result.data);
+                        if (element.rank = sortArr[i]) {
+                            $('#music-cards').append(
+                                `
+                                <ul class='card'>
+                                    <div class='mini_bg' style="background-image: url('${element.album.cover}');"></div>
+                                    <li><audio controls src=${element.preview}></audio></li>
+                                    <li>Titre:${element.title_short}</li>
+                                    <li>Artiste: ${element.artist.name}</li>
+                                    <li>Album:${element.album.title}</li>    
+                                </ul>
+                            `)
+                        }
+                    })
+                })
+            }
+        })   
+    })
 
